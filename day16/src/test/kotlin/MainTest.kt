ea6efc5c981a5f79869336d41098ce9ccbc7531d
Mike Neilens <mike.neilens@gmail.com>
2020-11-28 17:50:54 +0000

    setting up empty project for advent2020

---
+++ day16/src/test/kotlin/MainTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Test
+    fun `a test`() {
+        assertEquals(0,main())
+    }
+}
