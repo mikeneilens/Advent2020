8e96bc9d36480e51e0329e8e685bab311b1dba94
Mike Neilens <mike.neilens@gmail.com>
2020-12-01 08:32:20 +0000

    day1

--- day01/src/main/kotlin/Main.kt
+++ day01/src/main/kotlin/Main.kt
@@ -1,2 +1,13 @@
 
-fun main() = 0
+fun Pair<Int,Int>.sumIs2020() = (first+second) == 2020
+
+fun simplePermutation(list:List<Int>):List<Pair<Int,Int>> = list.flatMap{v1 -> list.map{v2 -> Pair(v1,v2)} }.filter{(v1,v2) -> v1 != v2}
+
+fun partOne(list:List<Int>) = simplePermutation(list).first{it.sumIs2020()}
+
+fun Triple<Int,Int,Int>.sumIs2020() = (first+second + third) == 2020
+
+fun simplePermutation3(list:List<Int>)
+        = list.flatMap{v1 -> list.flatMap{v2 -> list.map{v3 -> Triple(v1,v2,v3) }} }.filter{(v1,v2,v3) -> (v1 != v2 && v2 != v3 && v1 != v3) }
+
+fun partTwo(list:List<Int>) = simplePermutation3(list).first{it.sumIs2020()}
--- day01/src/test/kotlin/MainTest.kt
+++ day01/src/test/kotlin/MainTest.kt
@@ -1,9 +1,49 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class MainTest {
+
+    val testData = listOf(1721, 979, 366, 299, 675, 1456)
+
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `when two numners add to 2020 the result is true`() {
+        assertTrue(Pair(1000,1020).sumIs2020())
+    }
+
+    @Test
+    fun `simple permutation for list of (1,2,3) is (1,2),(1,3),(2,1),(2,3),(3,1),(3,2))`() {
+        val data = listOf(1,2,3)
+        val expectedResult = listOf(Pair(1,2),Pair(1,3),Pair(2,1),Pair(2,3),Pair(3,1),Pair(3,2))
+        assertEquals(expectedResult, simplePermutation(data))
+    }
+    @Test
+    fun `test data gives an answer of 1721,299`() {
+        val expectedResult = Pair(1721,299)
+        assertEquals(expectedResult, partOne(testData))
+    }
+
+    @Test
+    fun `part one`() {
+        val result = partOne(day1Data)
+        println("Result is ${result.first * result.second}")
+    }
+
+    @Test
+    fun `when three numners add to 2020 the result is true`() {
+        assertTrue(Triple(1000,1010,10).sumIs2020())
+    }
+
+    @Test
+    fun `simple permutation3 for list of (1,2,3) is (1,2,3), (1,3,2), (2,1,3), (2,3,1), (3,1,2), (3,2,1)`() {
+        val data = listOf(1,2,3)
+        val expectedResult = listOf(Pair(1,2),Pair(1,3),Pair(2,1),Pair(2,3),Pair(3,1),Pair(3,2))
+        assertEquals(expectedResult, simplePermutation3(data))
+    }
+
+    @Test
+    fun `part two`() {
+        val result = partTwo(day1Data)
+        println("Result is ${result.first * result.second * result.third}")
     }
 }
