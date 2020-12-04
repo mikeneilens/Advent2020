3f57a341fbfceeecbf3493e36c57c55ba7f26e27
Mike Neilens <mike.neilens@gmail.com>
2020-12-04 13:18:51 +0000

    day four correct some spelling mistakes

--- day04/src/main/kotlin/Main.kt
+++ day04/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 
-fun String.inRange(min:Int, max:Int):Boolean = toIntOrNull()?.let{ it >= min && it <= max } ?: false
+fun String.inRange(min:Int, max:Int):Boolean = toIntOrNull()?.let{ it in min..max } ?: false
 
-val passportfields = mapOf(
+val passportFields = mapOf(
     "byr" to validByr,
     "iyr" to validIyr,
     "eyr" to validEyr,
@@ -11,29 +11,29 @@ val passportfields = mapOf(
     "pid" to validPid,
     "cid" to validCid,
 )
-val northPoleCredentialFields = passportfields.filter{it.key != "cid"}
+val northPoleCredentialFields = passportFields.filter{it.key != "cid"}
 
 typealias Passport = String
 typealias Credentials = Map<String, String>
 
-const val passportSeperator = "\n\n"
+const val passportSeparator = "\n\n"
 
-fun String.toPassports() = split(passportSeperator)
+fun String.toPassports() = split(passportSeparator)
 
 fun Passport.toCredentials() =  replace("\n"," ").split(" ").map{ it.toKeyValuePair() }.toMap()
 
-fun String.toKeyValuePair(keyValueSeperator:String = ":"):Pair<String, String> {
-    val parts = split(keyValueSeperator)
+fun String.toKeyValuePair(keyValueSeparator:String = ":"):Pair<String, String> {
+    val parts = split(keyValueSeparator)
     return Pair(parts[0],parts[1])
 }
 
-fun Credentials.passportFieldsArePresent() = keys.containsAll(passportfields.keys)
+fun Credentials.passportFieldsArePresent() = keys.containsAll(passportFields.keys)
 
 fun Credentials.northPoleCredentialsFieldsArePresent() = keys.containsAll(northPoleCredentialFields.keys)
 
 fun String.passportsContainingRequiredFields():List<Credentials> = toPassports().map{it.toCredentials()}.filter{it.passportFieldsArePresent() || it.northPoleCredentialsFieldsArePresent()}
 
 fun Credentials.allValid() =
-    map{credential -> passportfields[credential.key]?.let{ rule -> rule(credential.value)}}.filter { it == false }.size == 0
+    map { credential -> passportFields[credential.key]?.let { rule -> rule(credential.value) } }.none { it == false }
 
 fun String.validPassports() = passportsContainingRequiredFields().map{it.allValid()}.filter{it}
