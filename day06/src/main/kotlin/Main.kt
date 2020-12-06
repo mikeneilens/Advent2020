1e157862eb838f56b51c529b2768a57ca3ae8697
Mike Neilens <mike.neilens@gmail.com>
2020-12-06 13:06:04 +0000

    day6

--- day06/src/main/kotlin/Main.kt
+++ day06/src/main/kotlin/Main.kt
@@ -2,8 +2,8 @@
 fun String.parseIntoGroups(): List<String> = split("\n\n")
 fun answersForGroup(group:String): List<String> = group.split("\n")
 
-fun uniqueAnswersInGroup(answers:List<String>):Int = answers.concatonated().toSet().size
-fun List<String>.concatonated() = joinToString("")
+fun uniqueAnswersInGroup(answers:List<String>):Int = answers.concatenated().toSet().size
+fun List<String>.concatenated() = joinToString("")
 
 fun String.uniqueAnswers() = parseIntoGroups()
     .map(::answersForGroup)
@@ -12,7 +12,7 @@ fun String.uniqueAnswers() = parseIntoGroups()
 fun questionsEveryOneAnswers(groupAnswers:List<String>) =
     ('a'..'z').filter{ groupAnswers.allPeopleInTheGroupAnswers(it) }
 
-fun List<String>.allPeopleInTheGroupAnswers(a:Char) = count{it.contains(a)} == size
+fun List<String>.allPeopleInTheGroupAnswers(a:Char) = all{it.contains(a)}
 
 fun String.sumOfQuestionsAnsweredByEachGroup() = parseIntoGroups()
     .map(::answersForGroup)
