f38c1e255f8d2d0bf2c35569ea3ce85aaef391fc
Mike Neilens <mike.neilens@gmail.com>
2020-12-07 18:38:06 +0000

    day07

--- day07/src/main/kotlin/Main.kt
+++ day07/src/main/kotlin/Main.kt
@@ -1,2 +1,41 @@
+import com.sun.deploy.security.ruleset.RuleSetParser
 
 fun main() = 0
+
+typealias Bag=String
+typealias BagAndRelatedBags=List<String>
+
+typealias Rule = Map<Bag, List<RelatedBag>>
+//data class Rule (val bag:Bag, val relatedBags:List<RelatedBag> )
+data class RelatedBag (val quantity:Int, val bag:String)
+
+fun toRule(s:String):Pair<Bag,List<RelatedBag>> {
+    val bagAndRelatedBags = s.removeSuffix(".").replace("bags","bag").split(" contain ")
+    return Pair(bagAndRelatedBags.bag, bagAndRelatedBags.allRelatedBags)
+}
+
+val BagAndRelatedBags.bag get() = get(0)
+val BagAndRelatedBags.allRelatedBags get() = if (hasNoRelatedBags()) emptyList() else someRelatedBags()
+fun BagAndRelatedBags.hasNoRelatedBags() = size < 2 || get(1).startsWith("no")
+fun List<String>.someRelatedBags() = if (size > 1) get(1).split(", ").map(::toRelatedBag) else emptyList()
+
+fun toRelatedBag(s:String) = RelatedBag(s.split(" ")[0].toInt(), s.split(" ").drop(1).joinToString(" "))
+
+fun String.parseToRules() = split("\n").map(::toRule).toMap()
+
+fun allBagsContaining(bag:Bag, rules:Rule ,bags:List<Bag> = emptyList()):List<Bag> {
+    val parentBags = bagsContaining(bag, rules)
+    if (parentBags.isEmpty()) return bags
+    else return parentBags + parentBags.flatMap{allBagsContaining(it,rules)}
+}
+
+fun bagsContaining(bag:String, rules:Rule) = rules.toList().filter { it.containsBag(bag)}.map{it.first}
+fun Pair<Bag, List<RelatedBag>>.containsBag(bag:Bag) = second.map{it.bag}.contains(bag)
+fun noOfBagsContaining(bag:Bag, rules:Rule) = allBagsContaining(bag, rules).distinct().size
+
+fun totalBags(bag:Bag, quantity:Int , rules:Rule ):Int {
+    val relatedBags = rules[bag]?.let{it } ?: emptyList()
+    if (relatedBags.isEmpty()) return quantity
+    return quantity + relatedBags.sumBy { relatedBag -> totalBags(relatedBag.bag, quantity * relatedBag.quantity, rules) }
+}
+
--- day07/src/test/kotlin/MainTest.kt
+++ day07/src/test/kotlin/MainTest.kt
@@ -2,8 +2,69 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MainTest {
+    val sampleData = """
+        light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags.
+    """.trimIndent()
+
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `test parsing a string into a rule containing related bags`() {
+        val data = "light red bags contain 1 bright white bag, 2 muted yellow bags."
+        val rules = toRule(data)
+        assertEquals("light red bag",rules.first)
+        assertEquals(listOf(RelatedBag(1,"bright white bag"),RelatedBag(2,"muted yellow bag")),rules.second)
+
+        val data2 = "bright white bags contain 1 shiny gold bag."
+        val rules2 = toRule(data2)
+        assertEquals("bright white bag", rules2.first)
+        assertEquals(listOf( RelatedBag(1,"shiny gold bag") ) , rules2.second)
+    }
+    @Test
+    fun `test parsing a string into a rule containing no related bags`() {
+        val data = "faded blue bags contain no other bags."
+        val rule = toRule(data)
+        assertEquals("faded blue bag", rule.first )
+        assertEquals(emptyList<RelatedBag>(), rule.second )
+    }
+    @Test
+    fun `test parsing sampleData into a list of Rules`() {
+        val rules = sampleData.parseToRules()
+        assertEquals(9, rules.size)
+    }
+    @Test
+    fun `test bag contained in one bag`() {
+        val rules = sampleData.parseToRules()
+        assertEquals(listOf("shiny gold bag"), bagsContaining("vibrant plum bag", rules))
+    }
+    @Test
+    fun `test all bag containing the bag using sample data`() {
+        val rules = sampleData.parseToRules()
+        assertEquals(listOf("bright white bag", "muted yellow bag", "light red bag", "dark orange bag"), allBagsContaining("shiny gold bag", rules).distinct())
+        assertEquals(4, noOfBagsContaining("shiny gold bag", rules))
+    }
+    @Test
+    fun `part one`() {
+        val rules = day07Data.parseToRules()
+        assertEquals(272, noOfBagsContaining("shiny gold bag", rules))
+    }
+    @Test
+    fun `test bags inside of gold bag`() {
+        val rules = sampleData.parseToRules()
+        assertEquals(7, totalBags("dark olive bag",1, rules) - 1)
+        assertEquals(11, totalBags("vibrant plum bag",1, rules) - 1)
+        println(" --")
+        assertEquals(32, totalBags("shiny gold bag",1, rules) - 1)
+    }
+    @Test
+    fun `part two`() {
+        val rules = day07Data.parseToRules()
+        assertEquals(172247, totalBags("shiny gold bag",1, rules))
     }
 }
