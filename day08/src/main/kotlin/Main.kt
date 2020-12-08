f172b5bd5f1596cd46bd691aa7b98b0ac4f699d6
Mike Neilens <mike.neilens@gmail.com>
2020-12-08 15:01:05 +0000

    day08

--- day08/src/main/kotlin/Main.kt
+++ day08/src/main/kotlin/Main.kt
@@ -1,2 +1,62 @@
 
-fun main() = 0
+var accumulator = 0
+
+data class ProgState(val ndx:Int, val accumulator:Int)
+
+fun nop(state:ProgState, argument:Int):ProgState = ProgState(state.ndx + 1, state.accumulator )
+fun acc(state:ProgState, argument:Int):ProgState = ProgState(state.ndx + 1, state.accumulator + argument )
+fun jmp(state:ProgState, argument:Int):ProgState = ProgState(state.ndx + argument, state.accumulator)
+
+typealias Rule = (ProgState, Int)->ProgState
+typealias Instruction = (ProgState) -> ProgState
+typealias Program = Map<Int, String>
+
+val ruleMap = mapOf<String, Rule>(
+    "nop" to ::nop,
+    "acc" to ::acc,
+    "jmp" to ::jmp
+)
+fun String.parseIntoProgram(): Program = split("\n").mapIndexed{i,v -> Pair(i,v)}.toMap()
+
+fun toInstruction(s:String):Instruction {
+    val parts= s.split(" ")
+    val rule = ruleMap.getValue(parts[0])
+    val argument = parts[1].toInt()
+    return {progState -> rule(progState, argument) }
+}
+
+fun Program.process( ):Pair<Int, Boolean> {
+    var progState = ProgState(0,0)
+    val indexesVisited = mutableListOf<Int>()
+    while (!indexesVisited.contains(progState.ndx) && get(progState.ndx) != null ) {
+        indexesVisited.add(progState.ndx)
+        val instruction = toInstruction(getValue(progState.ndx))
+        progState = instruction(progState)
+    }
+    return Pair(progState.accumulator,get(progState.ndx) == null)
+}
+
+fun Program.swappables() = toList().sortedBy { it.first }.filter{it.second.contains("jmp") || it.second.contains("nop")}
+
+fun partTwo(string:String):Pair<Int, Boolean> {
+    val program = string.parseIntoProgram()
+    val jmpOrNOPToSwap = program.swappables()
+    var oneToSwap = 0
+    while (oneToSwap < jmpOrNOPToSwap.size) {
+        val updatableProgram = program.toMutableMap()
+        replaceJmpOrNop(updatableProgram, jmpOrNOPToSwap, oneToSwap)
+        val (acc, finishedOK) = updatableProgram.process()
+        if (finishedOK) return Pair(acc, finishedOK)
+        oneToSwap++
+    }
+    return Pair(-1,false)
+}
+
+fun replaceJmpOrNop(program: MutableMap<Int, String>, swappables: List<Pair<Int, String>>, oneToSwap: Int) {
+    val swappableIndex = swappables[oneToSwap].first
+    val swappableInstruction = swappables[oneToSwap].second
+    program[swappableIndex] = if (swappableInstruction.contains("jmp"))
+        swappableInstruction.replace("jmp", "nop")
+    else
+        swappableInstruction.replace("nop", "jmp")
+}
--- day08/src/test/kotlin/MainTest.kt
+++ day08/src/test/kotlin/MainTest.kt
@@ -2,8 +2,41 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MainTest {
+    val sampleData = """
+        nop +0
+        acc +1
+        jmp +4
+        acc +3
+        jmp -3
+        acc -99
+        acc +1
+        jmp -4
+        acc +6
+    """.trimIndent()
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `parse sample data into a list of instructions`() {
+        assertEquals(9,sampleData.parseIntoProgram().size)
+    }
+    @Test
+    fun `process sample data until repeating instruction at same place`() {
+        val (acc, finishedOK) = sampleData.parseIntoProgram().process()
+        assertEquals(5, acc)
+        assertEquals(false, finishedOK)
+    }
+    @Test
+    fun `part one`() {
+        val (acc, finishedOK) = day08Data.parseIntoProgram().process()
+        assertEquals(1818, acc)
+        assertEquals(false, finishedOK)
+    }
+    @Test
+    fun `swapping jmp or nop using sample data`() {
+        val (acc, finishedOK) = partTwo(sampleData)
+        assertEquals(8, acc)
+    }
+    @Test
+    fun `part two`() {
+        val (acc, finishedOK) = partTwo(day08Data)
+        assertEquals(8, acc)
     }
 }
