6c19301b109ec569ab0293b24a56219790dadc75
Mike Neilens <mike.neilens@gmail.com>
2020-12-10 09:20:55 +0000

    day 10 part one

--- day10/src/main/kotlin/Main.kt
+++ day10/src/main/kotlin/Main.kt
@@ -1,2 +1,31 @@
+fun List<Int>.builtInJolt(): Int = (maxOrNull() ?: 0) + 3
 
-fun main() = 0
+fun List<Int>.allJolts() = (listOf(0) + this + builtInJolt()).sorted()
+
+fun List<Int>.createPairs():List<Pair<Int, Int>> = allJolts().zip(allJolts().drop(1))
+
+fun List<Pair<Int,Int>>.findDifferencesOfOneAndThree():Pair<Int, Int>  {
+    val differences = map{(j1,j2) -> j2 - j1 }
+    return Pair(differences.count{it == 1},differences.count{it == 3} )
+}
+fun dayOne(jolts:List<Int>) = jolts.createPairs().findDifferencesOfOneAndThree()
+
+fun List<Int>.combos(listSoFar:List<Int> = listOf()):List<List<Int>> {
+    if (isEmpty()) return listOf(listSoFar)
+    val last = if (listSoFar.isEmpty())  0 else listSoFar.last()
+    return take(3).flatMap {next ->
+        if (next - last <= 3 && next != last ) filter{it > next  }.combos(listSoFar + next ) else listOf()
+    }
+}
+fun partTwo(list:List<Int>):List<List<Int>> {
+    val result = list.allJolts().combos()
+    result.filter{it.size > 0 && it.last() == list.allJolts().last() }.take(10).forEach { println(it)  }
+    return result.filter{it.size > 0 && it.last() == list.allJolts().last() }
+}
+fun List<Int>.combos2() {
+     
+}
+
+
+
+
--- day10/src/test/kotlin/MainTest.kt
+++ day10/src/test/kotlin/MainTest.kt
@@ -2,8 +2,85 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MainTest {
+    val sample1 = listOf(16,
+        10,
+        15,
+        5,
+        1,
+        11,
+        7,
+        19,
+        6,
+        12,
+        4)
+
+    val sample2 = listOf(28,
+        33,
+        18,
+        42,
+        31,
+        14,
+        46,
+        20,
+        48,
+        47,
+        24,
+        23,
+        49,
+        45,
+        19,
+        38,
+        39,
+        11,
+        1,
+        32,
+        25,
+        35,
+        8,
+        17,
+        7,
+        9,
+        4,
+        2,
+        34,
+        10,
+        3)
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `built in jolt is 22`() {
+        assertEquals(22,sample1.builtInJolt())
     }
+    @Test
+    fun `list of differnces of 1 is 7 and difference of 3 is 5 for sample1`() {
+        val (diff1,diff3) = dayOne(sample1)
+        assertEquals(7, diff1)
+        assertEquals(5, diff3)
+    }
+    @Test
+    fun `list of differnces of 1 is 22 and difference of 3 is 10 for sample2`() {
+        val (diff1,diff3) = dayOne(sample2)
+        assertEquals(22, diff1)
+        assertEquals(10, diff3)
+    }
+    @Test
+    fun `part one`() {
+        val (diff1,diff3) = dayOne(day10Data)
+        assertEquals(74, diff1)
+        assertEquals(41, diff3)
+        println(diff1 * diff3)
+    }
+
+    @Test
+    fun`joltComobs for sample1 `() {
+        assertEquals(8,partTwo(sample1).size)
+    }
+    @Test
+    fun`joltComobs for sample2 `() {
+        assertEquals(8,partTwo(sample2).size)
+    }
+    @Test
+    fun`part two `() {
+        assertEquals(8,partTwo(day10Data).size)
+    }
+
 }
+
