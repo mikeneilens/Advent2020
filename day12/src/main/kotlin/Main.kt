649dd415cc7fdc68d7f2230c08c08ec39162f0ab
Mike Neilens <mike.neilens@gmail.com>
2020-12-12 12:44:38 +0000

    day 12

--- day12/src/main/kotlin/Main.kt
+++ day12/src/main/kotlin/Main.kt
@@ -1,2 +1,82 @@
-
 fun main() = 0
+
+data class Position(val x:Int, val y:Int) {
+    operator fun plus(other:Position):Position = Position(x + other.x, y + other.y)
+    operator fun times(other:Int):Position = Position(x * other, y * other)
+
+    fun rotateRightOnce():Position =  Position(y * -1, x)
+    fun rotateRight(d:Int):Position = if (d == 0)  this else rotateRightOnce().rotateRight(d - 90)
+    fun rotateLeftOnce():Position =  Position(y , x * -1)
+    fun rotateLeft(d:Int):Position = if (d == 0)  this else rotateLeftOnce().rotateLeft(d - 90)
+}
+enum class Orientation(val step:Position) {
+    North(Position(0,-1)),
+    South(Position(0,1)),
+    East(Position(1,0)),
+    West(Position(-1,0));
+
+    fun rotateRightOnce() = when(this) {
+        North -> East
+        South -> West
+        East -> South
+        West -> North
+    }
+    fun rotateRight(d:Int):Orientation = if (d == 0)  this else rotateRightOnce().rotateRight(d - 90)
+    fun rotateLeftOnce() = when(this) {
+        North -> West
+        South -> East
+        East -> North
+        West -> South
+    }
+    fun rotateLeft(d:Int):Orientation = if (d == 0)  this else rotateLeftOnce().rotateLeft(d - 90)
+}
+
+data class ShipStatus (val position:Position, val orientation:Orientation)
+
+fun ShipStatus.transform(instruction:String):ShipStatus {
+    val op = instruction[0]
+    val n = instruction.drop(1).toInt()
+    return when (op) {
+        'N' -> ShipStatus(position + Orientation.North.step * n, orientation)
+        'S' -> ShipStatus(position + Orientation.South.step * n, orientation)
+        'E' -> ShipStatus(position + Orientation.East.step * n, orientation)
+        'W' -> ShipStatus(position + Orientation.West.step * n, orientation)
+        'R' -> ShipStatus(position, orientation.rotateRight(n))
+        'L' -> ShipStatus(position, orientation.rotateLeft(n))
+        'F' -> ShipStatus(position + orientation.step * n, orientation)
+        else -> ShipStatus(position, orientation)
+    }
+}
+
+fun finalPosition(sampleData1: List<String>): ShipStatus {
+    var shipStatus = ShipStatus(Position(0,0),Orientation.East)
+    sampleData1.forEach{ instruction ->
+        shipStatus = shipStatus.transform(instruction)
+    }
+    return shipStatus
+}
+
+//part two
+data class ShipStatus2 (val position:Position, val orientation:Orientation, val wayPoint:Position)
+fun ShipStatus2.transform(instruction:String):ShipStatus2 {
+    val op = instruction[0]
+    val n = instruction.drop(1).toInt()
+    return when (op) {
+        'N' -> ShipStatus2(position, orientation, wayPoint + Orientation.North.step * n)
+        'S' -> ShipStatus2(position, orientation, wayPoint + Orientation.South.step * n)
+        'E' -> ShipStatus2(position, orientation, wayPoint + Orientation.East.step * n)
+        'W' -> ShipStatus2(position, orientation, wayPoint + Orientation.West.step * n)
+        'R' -> ShipStatus2(position, orientation, wayPoint.rotateRight(n))
+        'L' -> ShipStatus2(position, orientation, wayPoint.rotateLeft(n))
+        'F' -> ShipStatus2(position + wayPoint * n, orientation, wayPoint )
+        else -> ShipStatus2(position, orientation, wayPoint)
+    }
+}
+
+fun finalPosition2(sampleData1: List<String>): ShipStatus2 {
+    var shipStatus = ShipStatus2(Position(0,0),Orientation.East, Position(10,-1))
+    sampleData1.forEach{ instruction ->
+        shipStatus = shipStatus.transform(instruction)
+    }
+    return shipStatus
+}
--- day12/src/test/kotlin/MainTest.kt
+++ day12/src/test/kotlin/MainTest.kt
@@ -1,9 +1,74 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.math.abs
 
 class MainTest {
+    val sampleData1 = """
+        F10
+        N3
+        F7
+        R90
+        F11
+    """.trimIndent().split("\n")
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `adding two positions`() {
+        val p1 = Position(1,2)
+        val p2 = Position(6,8)
+        assertEquals(Position(7,10), p1 + p2)
+    }
+    @Test
+    fun `multiplying a position by 7`() {
+        val p1 = Position(6,8)
+        assertEquals(Position(42,56), p1 * 7)
+    }
+    @Test
+    fun `rotating 270 degrees`() {
+        assertEquals(Orientation.East, Orientation.North.rotateRight(90))
+        assertEquals(Orientation.South, Orientation.North.rotateRight(180))
+        assertEquals(Orientation.West , Orientation.North.rotateRight(270) )
+        assertEquals(Orientation.West , Orientation.North.rotateLeft(90) )
+        assertEquals(Orientation.South , Orientation.North.rotateLeft(180) )
+        assertEquals(Orientation.East , Orientation.North.rotateLeft(270) )
+    }
+    @Test
+    fun `final position of sampleData`() {
+        val (position,orientiation) = finalPosition(sampleData1)
+        assertEquals(Position(17,8), position)
+        assertEquals(Orientation.South, orientiation)
+        assertEquals(25, abs(position.x) + abs(position.y))
+    }
+    @Test
+    fun `part one`() {
+        val (position,orientiation) = finalPosition(day12Data)
+        assertEquals(Position(426,478), position)
+        assertEquals(Orientation.North, orientiation)
+        assertEquals(904, abs(position.x) + abs(position.y))
+    }
+    @Test
+    fun `rotating  wayPoint 360 degrees`() {
+        assertEquals(Position(4,10),   Position(10,-4).rotateRight(90))
+        assertEquals(Position(-10,4),   Position(10,-4).rotateRight(180))
+        assertEquals(Position(-4,-10),   Position(10,-4).rotateRight(270))
+        assertEquals(Position(10,-4),   Position(10,-4).rotateRight(360))
+
+        assertEquals(Position(-4,-10),   Position(10,-4).rotateLeft(90))
+        assertEquals(Position(-10,4),   Position(10,-4).rotateLeft(180))
+        assertEquals(Position(4,10),   Position(10,-4).rotateLeft(270))
+        assertEquals(Position(10,-4),   Position(10,-4).rotateLeft(360))
+
+    }
+    @Test
+    fun `final position part 2 of sampleData`() {
+        val (position,orientiation, wayPoint) = finalPosition2(sampleData1)
+        assertEquals(Position(214,72), position)
+        assertEquals(Orientation.East, orientiation)
+        assertEquals(286, abs(position.x) + abs(position.y))
+    }
+    @Test
+    fun `part two`() {
+        val (position,orientiation, wayPoint) = finalPosition2(day12Data)
+        assertEquals(Position(-10766,7981), position)
+        assertEquals(Orientation.East, orientiation)
+        assertEquals(18747, abs(position.x) + abs(position.y))
     }
 }
