1ddc8505a3dcf14335ec3455484541fa192c99c1
Mike Neilens <mike.neilens@gmail.com>
2020-12-13 15:24:13 +0000

    day 13

--- day13/src/main/kotlin/Main.kt
+++ day13/src/main/kotlin/Main.kt
@@ -1,2 +1,50 @@
+fun parseData(data: List<String>) =
+    Pair(data[0].toInt(),
+        data[1].split(",").filter{it!="x"}.map{it.toInt()}
+    )
 
-fun main() = 0
+fun findBestBus(earliestDepart:Int, busIds:List<Int>):Pair<Int, Int> {
+    var delay = 0
+    while (true) {
+        busIds.forEach { busId ->
+            if (busDepartsAtTime(busId, earliestDepart + delay)) return Pair(busId, earliestDepart+ delay)
+        }
+        delay++
+    }
+}
+fun busDepartsAtTime(busId:Int, time:Int) = (time % busId) == 0
+
+fun partOne(data:List<String>):Pair<Int, Int> = findBestBus(parseData(data).first, parseData(data).second )
+
+data class BusInfo(val busId:Int, val interval:Int)
+
+fun transform(p:Pair<List<BusInfo>, Int>, data:String):Pair<List<BusInfo>,Int> {
+    val (listOfBusInfo, busCount) = p
+    return if (data == "x") Pair(listOfBusInfo, busCount + 1)
+    else Pair(listOfBusInfo + BusInfo(data.toInt(), busCount + 1), busCount + 1)
+}
+fun parseData2(data:List<String>):Pair<List<BusInfo>,Int> {
+    val initial = Pair(listOf(BusInfo(data[1].split(",")[0].toInt(),0)),0)
+    return data[1].split(",").drop(1).fold(initial, ::transform)
+}
+
+fun partTwo(data:List<String>): Long {
+    val (listOfBusInfo, _) = parseData2(data)
+
+    var totalTime =  listOfBusInfo[0].busId.toLong()
+    var minTimeincrement = 1L
+    for (busInfo in listOfBusInfo) {
+        totalTime = nextValidtimeForBus(busInfo, totalTime, minTimeincrement)
+        minTimeincrement *= busInfo.busId
+    }
+    return totalTime
+}
+fun timeIsValidForBus(busInfo:BusInfo, time:Long ):Boolean = ( (time + busInfo.interval) % busInfo.busId == 0L)
+
+fun nextValidtimeForBus(busInfo:BusInfo, startingTime:Long, timeIncrement:Long ):Long {
+    var time = startingTime + timeIncrement
+    while (true) {
+        if ( timeIsValidForBus(busInfo, time))  return time
+        time = time + timeIncrement
+    }
+}
--- day13/src/test/kotlin/MainTest.kt
+++ day13/src/test/kotlin/MainTest.kt
@@ -1,9 +1,59 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class MainTest {
+    val sampleData = """
+        939
+        7,13,x,x,59,x,31,19
+    """.trimIndent().split("\n")
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `parse data into an integer and list of integers`() {
+        val (earliestDepart, busIds) = parseData(sampleData)
+        assertEquals(939, earliestDepart)
+        assertEquals(listOf(7,13,59,31,19), busIds)
+    }
+    @Test
+    fun `finding best bus from sample data`() {
+        val (earliestTime, _) = parseData(sampleData)
+        val (busId, time) = partOne(sampleData)
+        assertEquals(59, busId)
+        assertEquals(944, time)
+        assertEquals(295, busId * (time - earliestTime))
+    }
+    @Test
+    fun `part one`() {
+        val (earliestTime, _) = parseData(day13Data)
+        val (busId, time) = partOne(day13Data)
+        assertEquals(17, busId)
+        assertEquals(1000059, time)
+        assertEquals(119, busId * (time - earliestTime))
+    }
+    //part two
+    @Test
+    fun `parse data into a list of busids and intervals`() {
+        val expectedResult = listOf(
+            BusInfo(7,0),
+            BusInfo(13,1),
+            BusInfo(59,4),
+            BusInfo(31,6),
+            BusInfo(19,7))
+        val (listOfBusInfo,_)  = parseData2(sampleData)
+        assertEquals( expectedResult, listOfBusInfo)
+    }
+
+    @Test
+    fun `find valid departure time for busses in sample data`() {
+        assertEquals(1068781, partTwo(sampleData))
+    }
+    @Test
+    fun `find valid departure time for busses in other sample data`() {
+        assertEquals(754018, partTwo(listOf("","67,7,59,61")))
+        assertEquals(779210, partTwo(listOf("","67,x,7,59,61")))
+        assertEquals(1202161486, partTwo(listOf("","1789,37,47,1889")))
+    }
+    @Test
+    fun `part two`() {
+        assertEquals(1106724616194525, partTwo( day13Data))
     }
 }
