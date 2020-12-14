42788db44a0092ec45191dd2aeb53c54ccbd0849
Mike Neilens <mike.neilens@gmail.com>
2020-12-14 16:25:23 +0000

    day 14

--- day14/src/main/kotlin/Main.kt
+++ day14/src/main/kotlin/Main.kt
@@ -1,2 +1,89 @@
+import kotlin.math.pow
 
-fun main() = 0
+data class Instruction(val address:Long, val value:Long) {
+    fun applyMaskToValue(mask:String):Instruction = Instruction(address, value.applyMask(mask))
+
+    fun updateMemory(memory:MutableMap<Long,Long>, mask:String) {
+        memory[address] = applyMaskToValue(mask).value.toLong()
+    }
+    fun applyMaskToAddress(mask:String) =
+        convertMask(mask).map{m ->
+            Instruction(address.applyMask(m), value)
+        }
+    fun updateMemory2(memory:MutableMap<Long,Long>, mask:String) {
+        val instructions = applyMaskToAddress(mask)
+        instructions.forEach { memory[it.address] = it.value }
+    }
+}
+
+fun Long.applyMask(mask: String): Long {
+    val binary = toBinary()
+    val zipped = mask.zip(binary)
+    val maskedValue = zipped.map { p -> if (p.first != 'X') p.first else p.second }.joinToString("").binaryToLong()
+    return maskedValue
+}
+
+data class Program(val mask:String, val instructions:List<Instruction> ) {
+    fun process(memory:MutableMap<Long, Long>) {
+        instructions.forEach { it.updateMemory(memory, mask) }
+    }
+    fun process2(memory:MutableMap<Long, Long>) {
+        instructions.forEach { it.updateMemory2(memory, mask) }
+    }
+
+}
+
+//This is horrible!
+fun List<String>.parse():List<Program> {
+    val chunks = mutableListOf<List<String>>()
+    var chunk = mutableListOf<String>()
+    forEach { line ->
+        if (line.startsWith("mask")) {
+            if (!chunk.isEmpty()) {
+                chunks.add(chunk)
+            }
+            chunk = mutableListOf<String>(line)
+        } else {
+            chunk.add(line)
+        }
+    }
+    if (!chunk.isEmpty()) {
+        chunks.add(chunk)
+        chunk = mutableListOf<String>()
+    }
+    val programs = chunks.map{
+        val mask = it.first().split(" = ")[1]
+        val instructions = it.drop(1).map{ line ->
+                val splitLine = line.split(" = ")
+                val value = splitLine[1].toLong()
+                val address = splitLine[0].removePrefix("mem[").removeSuffix("]").toLong()
+                Instruction(address, value)
+        }
+        Program(mask, instructions)
+    }
+
+    return programs
+}
+
+//binary conversion. This is horrible!
+
+fun String.binaryToLong(chars:String = "1") :Long =
+    reversed().mapIndexed{index, digit -> if ( chars.contains(digit)) pow(2, index) else 0 }.sum()
+fun pow(a:Int, b:Int) = a.toDouble().pow(b.toDouble()).toLong()
+
+fun Long.toBinary():String =
+    (0..35).reversed().fold(Pair(this,"")){ (total, result), ndx ->
+        val bin = 2.toDouble().pow(ndx).toLong()
+        if ( (total / bin) >= 1L) Pair(total - bin, result + "1")
+        else Pair(total, result + "0")
+    }.second
+
+//part two
+fun formatMask(mask:String) = mask.replace('X','?').replace('0','X')
+fun convertMask(mask:String) = convertQuestionMark( formatMask(mask))
+fun convertQuestionMark(mask: String): List<String> {
+    if (!mask.contains('?')) return listOf(mask)
+    return listOf(mask.replaceFirst('?','0'),mask.replaceFirst('?','1'))
+        .flatMap{convertQuestionMark(it)}
+}
+
--- day14/src/test/kotlin/MainTest.kt
+++ day14/src/test/kotlin/MainTest.kt
@@ -2,8 +2,104 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MainTest {
+    val sampleData = """
+        mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+        mem[8] = 11
+        mem[7] = 101
+        mem[8] = 0
+    """.trimIndent().split("\n")
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `parsing input`() {
+        val expectedResult = listOf(
+            Program("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+            listOf(Instruction(8,11), Instruction(7,101),Instruction(8,0))
+        ))
+        assertEquals(expectedResult, sampleData.parse() )
     }
+    @Test
+    fun `binary to integer`() {
+        val result = "000000000000000000000000000001001001".binaryToLong()
+        assertEquals(73, result)
+    }
+    @Test
+    fun `integer to binary`() {
+        val result = 73L.toBinary()
+        assertEquals("000000000000000000000000000001001001", result)
+    }
+    @Test
+    fun `applying masks`() {
+        val mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+        val result1 = Instruction(8,11).applyMaskToValue(mask)
+        assertEquals(Instruction(8,73), result1)
+        val result2 = Instruction(7,101).applyMaskToValue(mask)
+        assertEquals(Instruction(7,101), result2)
+        val result3 = Instruction(8,0).applyMaskToValue(mask)
+        assertEquals(Instruction(8,64), result3)
+    }
+    @Test
+    fun `processing sample data program`() {
+        val memory = mutableMapOf<Long, Long>()
+        sampleData.parse().forEach{program ->  program.process(memory)}
+        assertEquals(165, memory.values.sum())
+    }
+    @Test
+    fun `part one`() {
+        val memory = mutableMapOf<Long, Long>()
+        day14Data.parse().forEach{program ->  program.process(memory)}
+        assertEquals(7817357407588, memory.values.sum())
+    }
+
+    //part two
+    @Test
+    fun `a mask containing one X is converted to a list of maps containing two masks`() {
+        val result:List<String> = convertMask("00000X11111")
+        val expectedResult:List<String> = listOf("XXXXX011111","XXXXX111111")
+        assertEquals(expectedResult, result)
+    }
+    @Test
+    fun `a mask containing two X is converted to a list of maps containing four masks`() {
+        val result:List<String> = convertMask("00000X11X11")
+        val expectedResult:List<String> = listOf("XXXXX011011","XXXXX011111","XXXXX111011","XXXXX111111")
+        assertEquals(expectedResult, result)
+    }
+    @Test
+    fun `applying a mask to an address`() {
+        val instruction = Instruction(42,100)
+        val results = instruction.applyMaskToAddress("000000000000000000000000000000X1001X")
+        val expectedResults = listOf(
+            Instruction(26, 100), Instruction(27, 100), Instruction(58, 100), Instruction(59, 100)
+        )
+        assertEquals(expectedResults, results)
+    }
+    @Test
+    fun `processing sample data program for part two`() {
+        val sampleData = """
+            mask = 00000000000000000000000000000000X0XX
+            mem[26] = 1
+        """.trimIndent().split("\n")
+        val memory = mutableMapOf<Long, Long>()
+        sampleData.parse().forEach{program ->  program.process2(memory)}
+
+        assertEquals(listOf(16L,17L,18L,19L,24L,25L,26L,27L), memory.keys.toList())
+    }
+    @Test
+    fun `processing sample full program for part two`() {
+        val sampleData = """
+            mask = 000000000000000000000000000000X1001X
+            mem[42] = 100
+            mask = 00000000000000000000000000000000X0XX
+            mem[26] = 1
+        """.trimIndent().split("\n")
+        val memory = mutableMapOf<Long, Long>()
+        sampleData.parse().forEach{program ->  program.process2(memory)}
+
+        assertEquals(208, memory.values.sum())
+    }
+    @Test
+    fun `part two`() {
+        val memory = mutableMapOf<Long, Long>()
+        day14Data.parse().forEach{program ->  program.process2(memory)}
+        assertEquals(4335927555692, memory.values.sum())
+    }
+
 }
