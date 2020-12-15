6202af11442d487958b7f95a1d0e1753fda56c3f
Mike Neilens <mike.neilens@gmail.com>
2020-12-15 18:05:06 +0000

    day 15

--- day15/src/main/kotlin/Main.kt
+++ day15/src/main/kotlin/Main.kt
@@ -1,2 +1,28 @@
 
-fun main() = 0
+fun nextNumber(numbers:List<Int>):Int {
+    val lastNumber = numbers.last()
+    val highestIndex = numbers.dropLast(1).lastIndexOf(lastNumber)
+    return if (highestIndex == -1)  0
+    else numbers.size - highestIndex - 1
+}
+
+tailrec fun numberAt(i: Int, numbers: List<Int>): Int {
+    return if (numbers.size >= i ) {println(numbers);   numbers[i-1]}
+    else numberAt(i, numbers + nextNumber(numbers) )
+}
+
+//part two
+val spokenValues = mutableMapOf<Int,Int>()
+
+fun updateMapAndReturnNewValue(lastValue:Int, position:Int, spokenValues:MutableMap<Int,Int>):Int {
+    val newValue = (spokenValues[lastValue] ?: position)
+    spokenValues[lastValue] = position
+    return position - newValue
+}
+fun process(lastValue:Int, start:Int, finish:Int, spokenValues:MutableMap<Int,Int>):Int {
+    var value = lastValue
+    for (position in start..finish) {
+        value = updateMapAndReturnNewValue(value, position, spokenValues)
+    }
+    return value
+}
--- day15/src/test/kotlin/MainTest.kt
+++ day15/src/test/kotlin/MainTest.kt
@@ -3,7 +3,68 @@ import org.junit.jupiter.api.Test
 
 class MainTest {
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `with 0 3 6 next number is 0`() {
+        val testList = listOf(
+            Pair(0, listOf(0,3,6)),
+            Pair(3,listOf(0,3,6,0)),
+            Pair(3,listOf(0,3,6,0,3)),
+            Pair(1,listOf(0,3,6,0,3,3)),
+            Pair(0,listOf(0,3,6,0,3,3,1)),
+            Pair(4,listOf(0,3,6,0,3,3,1,0)),
+            Pair(0,listOf(0,3,6,0,3,3,1,0,4)),
+        )
+       testList.forEach{ (result, input) ->
+            assertEquals(result, nextNumber(input))
+        }
+
+    }
+    @Test
+    fun `number at index 2020 is 436`() {
+        val result:Int = numberAt(2020, listOf(0,3,6))
+        assertEquals(436, result)
+    }
+    @Test
+    fun `part one`() {
+        val result = numberAt(2020, listOf(2,1,10,11,0,6))
+        assertEquals(232, result)
+    }
+    //part two
+    @Test
+    fun `with 0 3 6 next number is 0 for part two`() {
+        val map = mutableMapOf(0 to 0,3 to 1)
+        val lastNumber = 6
+        assertEquals(0, updateMapAndReturnNewValue(lastNumber,2,map))
+        assertEquals(2, map[6])
+        assertEquals(3, updateMapAndReturnNewValue(0,3,map))
+        assertEquals(3, map[0])
+        assertEquals(3, updateMapAndReturnNewValue(3,4,map))
+        assertEquals(4, map[3])
+    }
+    @Test
+    fun `processing a small range a values`() {
+        val map1 = mutableMapOf(0 to 0,3 to 1, 6 to 2)
+        val result1 = process(0,3,4, map1)
+        assertEquals(3, result1)
+    }
+    @Test
+    fun `processing a large range a values`() {
+        val map = mutableMapOf(0 to 0,3 to 1, 6 to 2)
+        val result = process(0,3,2018, map)
+        assertEquals(436, result)
+        val map1 = mutableMapOf(0 to 0,3 to 1, 6 to 2)
+        val result1 = process(0,3,30000000 - 2, map1)
+        assertEquals(175594, result1)
+    }
+    @Test
+    fun `processing part one using part two process`() {
+        val map = mutableMapOf(2 to 0, 1 to 1, 10 to 2, 11 to 3, 0 to 4, 6 to 5 )
+        val result = process(0,6,2020 - 2, map)
+        assertEquals(232, result)
+    }
+    @Test
+    fun `part two`() {
+        val map = mutableMapOf(2 to 0, 1 to 1, 10 to 2, 11 to 3, 0 to 4, 6 to 5 )
+        val result = process(0,6,30000000 - 2, map)
+        assertEquals(18929178, result)
     }
 }
