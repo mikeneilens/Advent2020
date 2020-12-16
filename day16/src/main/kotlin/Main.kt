5f28c43d9307ad373103f0022a7b10b30de56ab4
Mike Neilens <mike.neilens@gmail.com>
2020-12-16 15:16:22 +0000

    day 16

--- day16/src/main/kotlin/Main.kt
+++ day16/src/main/kotlin/Main.kt
@@ -1,2 +1,71 @@
 
-fun main() = 0
+
+fun List<String>.rulesLines() = takeWhile { it.isNotEmpty() }
+fun List<String>.yourTicketLine() = dropWhile { it != "your ticket:" }.drop(1).first()
+fun List<String>.nearbyTicketLines() = dropWhile{it!= "nearby tickets:"}.drop(1)
+
+fun checkRanges(value:Int, s1:Int,e1:Int, s2:Int, e2:Int ) = value in s1..e1 || value in s2..e2
+
+fun getRangesOnALine(ruleLine:String):List<Int> {
+    val rightOfColon = ruleLine.split(": ")[1]
+    val textr1 = rightOfColon.split(" or ")[0]
+    val textr2 = rightOfColon.split(" or ")[1]
+    val s1 = textr1.split("-")[0].toInt()
+    val e1 = textr1.split("-")[1].toInt()
+    val s2 = textr2.split("-")[0].toInt()
+    val e2 = textr2.split("-")[1].toInt()
+    return listOf(s1,e1,s2,e2)
+}
+fun makeRuleForARange(l:List<Int>) =  { value:Int -> checkRanges(value, l[0],l[1],l[2],l[3]) }
+
+fun List<String>.toRules() = rulesLines().map(::getRangesOnALine).map(::makeRuleForARange)
+
+fun List<String>.invalidValues():List<Int> {
+    val allNearbyValues = nearbyTicketLines().flatMap{line -> line.split(",").map{ it.toInt() } }
+    val rules = toRules()
+    val invalidValues = allNearbyValues.fold(listOf<Int>()) {result, value ->
+        if (rules.none{it(value)}) result + value
+        else result
+    }
+    return invalidValues
+}
+//part two
+fun String.ticketLineToInt() = split(",").map{ it.toInt() }
+
+fun List<String>.validTickets():List<List<Int>> {
+    val rules = toRules()
+    return  nearbyTicketLines()
+        .map(String::ticketLineToInt)
+        .filter{values -> values.allValuesComplyWithRules(rules)}
+}
+fun List<Int>.allValuesComplyWithRules(rules:List<(Int)->Boolean>) =
+    all{value -> rules.any{it(value)}}
+
+fun List<Int>.compliesWith(rule:(Int)->Boolean) = all{rule(it)}
+
+fun potentialRulesForEachCol(data:List<String>):List<List<Int>> {
+    val validTickets = data.validTickets()
+    val lastCol = validTickets.first().lastIndex
+    val rules = data.toRules()
+    val valuesInEachColum = (0..lastCol).map{col-> validTickets.map{it[col]}}
+    val result = mutableListOf<List<Int>>()
+    valuesInEachColum.forEachIndexed { col, values ->
+        val rulesForColum = mutableListOf<Int>()
+        rules.forEachIndexed { ruleNdx, rule ->
+            if (values.compliesWith (rule)) rulesForColum.add(ruleNdx)
+        }
+        result.add(rulesForColum)
+    }
+    return result
+}
+fun List<List<Int>>.rationalise():List<List<Int>>   {
+    if (all{it.size == 1}) return this
+    val singleRuleCols = filter{it.size == 1}
+    return map{rules -> if(rules.size == 1) rules
+                        else rules.filter{rule ->
+        !singleRuleCols.map{it.first()}.contains(rule)} }.rationalise()
+}
+fun rulesDecoded(data:List<String>) =
+     potentialRulesForEachCol(data).rationalise().flatMap{it}.mapIndexed{index, rulendx ->
+         Pair(index, data.rulesLines()[rulendx])}
+
--- day16/src/test/kotlin/MainTest.kt
+++ day16/src/test/kotlin/MainTest.kt
@@ -1,9 +1,111 @@
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class MainTest {
+    val sampleData = """
+        class: 1-3 or 5-7
+        row: 6-11 or 33-44
+        seat: 13-40 or 45-50
+
+        your ticket:
+        7,1,14
+
+        nearby tickets:
+        7,3,47
+        40,4,50
+        55,2,20
+        38,6,12
+    """.trimIndent().split("\n")
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `getting rulesLines, yourTicketLines and nearByTicketLines from sample data`() {
+       val ruleLines = sampleData.rulesLines()
+        assertEquals(listOf("class: 1-3 or 5-7", "row: 6-11 or 33-44", "seat: 13-40 or 45-50"), ruleLines)
+        val yourTicketLine = sampleData.yourTicketLine()
+        assertEquals("7,1,14", yourTicketLine)
+        val nearByTicketLines = sampleData.nearbyTicketLines()
+        assertEquals(listOf("7,3,47", "40,4,50", "55,2,20", "38,6,12"), nearByTicketLines)
     }
+    @Test
+    fun `test transforming a rule to a list of ranges`() {
+        val result = getRangesOnALine("seat: 13-40 or 45-50")
+        assertEquals(listOf(13,40,45,50),result)
+    }
+    @Test
+    fun `changing ranges into a rule`() {
+        val rule =  makeRuleForARange(listOf(13,40,45,50))
+        assertFalse(rule(12))
+        assertTrue(rule(13))
+        assertTrue(rule(40))
+        assertFalse(rule(41))
+        assertTrue(rule(45))
+        assertTrue(rule(50))
+        assertFalse(rule(51))
+    }
+    @Test
+    fun `create rules for sample data`() {
+        val rules = sampleData.toRules()
+        assertTrue(rules[2](13))
+    }
+    @Test
+    fun `invalid values on nearyby tickets in sample data is 4,55,12`() {
+        assertEquals(listOf(4,55,12), sampleData.invalidValues())
+        assertEquals(71, sampleData.invalidValues().sum())
+    }
+    @Test
+    fun `part one`() {
+        assertEquals(19070, day16Data.invalidValues().sum())
+    }
+
+    val partTwoData = """
+        class: 0-1 or 4-19
+        row: 0-5 or 8-19
+        seat: 0-13 or 16-19
+
+        your ticket:
+        11,12,13
+
+        nearby tickets:
+        3,9,18
+        15,1,5
+        5,14,9
+    """.trimIndent().split("\n")
+
+    @Test
+    fun `all values for a ticket comply with rules`() {
+        val rules = sampleData.toRules()
+        assertTrue(listOf(7,3,47).allValuesComplyWithRules(rules))
+        assertFalse(listOf(40,4,50).allValuesComplyWithRules(rules))
+        assertFalse(listOf(55,2,20).allValuesComplyWithRules(rules))
+        assertFalse(listOf(38,6,12).allValuesComplyWithRules(rules))
+    }
+    @Test
+    fun `only tickets that comply with the rules`() {
+        val rules = sampleData.toRules()
+        assertEquals(listOf(listOf(7,3,47)), sampleData.validTickets())
+    }
+    @Test
+    fun `potential rules for each column is (1), (0,1) and (0,1,2)`() {
+        assertEquals(listOf(listOf(1),listOf(0,1), listOf(0,1,2)) , potentialRulesForEachCol(partTwoData))
+    }
+    @Test
+    fun `rationalising rules for each col`(){
+        val rulesForEachCol = listOf(listOf(1),listOf(0,1), listOf(0,1,2))
+        assertEquals(listOf(listOf(1),listOf(0),listOf(2)), rulesForEachCol.rationalise())
+    }
+    @Test
+    fun `decoding rules`() {
+        println(rulesDecoded(partTwoData) )
+    }
+    @Test
+    fun `part two`() {
+        val decoded = rulesDecoded(day16Data)
+        val descriptionRules = decoded.filter{it.second.startsWith("depart")}
+        val myTicket = day16Data.yourTicketLine().ticketLineToInt()
+        val answer = descriptionRules.map{ (ndx, rule) ->
+            println("$rule ${myTicket[ndx]}")
+            myTicket[ndx]
+        }.fold(1L){acc, v -> acc * v}
+        println(answer)
+    }
+
 }
