68042f8e2a0ba9ec5cb5b30713d961c9a3c6b16e
Mike Neilens <mike.neilens@gmail.com>
2020-12-17 22:00:29 +0000

    day17 refactor

--- day17/src/main/kotlin/Main.kt
+++ day17/src/main/kotlin/Main.kt
@@ -1,35 +1,33 @@
 import kotlin.math.pow
-typealias PositionL = List<Int>
 
-operator fun PositionL.plus(other:PositionL) = this.zip(other).map{it.first + it.second}
+typealias ConwayCubes = MutableMap<Position, State>
 
-val PositionL.x:Int get() = get(0)
-val PositionL.y:Int get() = get(1)
-val PositionL.z:Int get() = get(2)
-val PositionL.w:Int get() = get(3)
+typealias Position = List<Int>
+operator fun Position.plus(other:Position) = this.zip(other).map{it.first + it.second}
 
-fun surroundingPositions(dimensions:Int):List<PositionL> =
+fun surroundingPositions(dimensions:Int):List<Position> =
     (0 until (3.0.pow(dimensions ).toInt())).map{
         (0 until dimensions).map{ n ->
             (it / 3.0.pow(n).toInt() % 3 - 1 )
         }
     }.filter{ d -> !d.all{it == 0}}
 
-fun updateCube(conwayCubes: ConwayCubes, data:List<String>, dimensions:Int) =
+fun createConwayCubes(data:List<String>, dimensions:Int):ConwayCubes{
+    val conwayCubes:ConwayCubes = mutableMapOf()
     (data.first().indices).forEach{ x -> (0..data.lastIndex).forEach{ y->
         conwayCubes[(0 until dimensions).map{ when (it) {0 -> x 1 -> y else ->0} }] = data[y][x].toState() } }
+    return conwayCubes
+}
 
 enum class State(val text:Char) {
     Active('#'), Inactive('.')
 }
 fun Char.toState() = if (this == State.Active.text) State.Active else State.Inactive
 
-typealias ConwayCubes = MutableMap<PositionL, State>
-
-fun ConwayCubes.activeNeighbours(position:PositionL, surroundingPositions:List<List<Int>>) =
+fun ConwayCubes.activeNeighbours(position:Position, surroundingPositions:List<List<Int>>) =
     surroundingPositions.count { surroundingPosition -> get(position + surroundingPosition) == State.Active  }
 
-fun ConwayCubes.newState(position:PositionL,surroundingPositions:List<List<Int>>):State {
+fun ConwayCubes.newState(position:Position, surroundingPositions:List<List<Int>>):State {
     val activeNeighbours = activeNeighbours(position, surroundingPositions)
     return if (get(position) == State.Active)
         if (activeNeighbours ==2 || activeNeighbours == 3) State.Active else State.Inactive
@@ -40,13 +38,10 @@ fun ConwayCubes.newState(position:PositionL,surroundingPositions:List<List<Int>>
 fun ConwayCubes.repeatCycles(noOfCycles:Int, dimensions: Int):ConwayCubes =
     if (noOfCycles <= 0) this else cycle(dimensions).repeatCycles(noOfCycles -1, dimensions)
 
-
-//part two
-
 fun ConwayCubes.cycle(dimensions:Int):ConwayCubes {
     val surroundingPositions = surroundingPositions(dimensions)
-    val minAndMaxes = (0..(dimensions-1)).map{d -> (keys.minOf { it[d] } -1)..(keys.maxOf { it[d] } +1)}.map{it.toList()}
-    val positions = getPositions(minAndMaxes, dimensions)
+    val minAndMaxes = (0 until dimensions).map{d -> (keys.minOf { it[d] } -1)..(keys.maxOf { it[d] } +1)}.map{it.toList()}
+    val positions = getPositions(minAndMaxes)
     val newConwayCubes = toMutableMap()
     positions.forEach { position ->
         newConwayCubes [position] = newState(position, surroundingPositions)
@@ -54,14 +49,12 @@ fun ConwayCubes.cycle(dimensions:Int):ConwayCubes {
     return newConwayCubes
 }
 
-fun getPositions(minAndMaxes: List<List<Int>>, dimensions: Int): List<PositionL> {
-    var n = 0
-    var combos = minAndMaxes[n++].map { listOf(it) }
-    while (n < dimensions) {
-        combos = combos.flatMap { first -> minAndMaxes[n].map { second -> first + second } }
-        n++
+fun getPositions(minAndMaxes: List<List<Int>>): List<Position> =
+    minAndMaxes.foldIndexed(listOf()){ index, result, minAndMax ->
+        if (index == 0) minAndMax.map { listOf(it) }
+        else result.flatMap { first -> minAndMax.map { second -> first + second } }
     }
-    return combos
-}
 
+fun List<String>.process(noOfCycles:Int,dimensions:Int) =
+    createConwayCubes(this,dimensions).repeatCycles(noOfCycles, dimensions)
 
--- day17/src/test/kotlin/MainTest.kt
+++ day17/src/test/kotlin/MainTest.kt
@@ -1,3 +1,4 @@
+import jdk.nashorn.internal.ir.annotations.Ignore
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -11,8 +12,8 @@ class MainTest {
             ###
         """.trimIndent().split("\n")
 
-        val conwayCubes:ConwayCubes = mutableMapOf()
-        updateCube(conwayCubes,data, dimensions = 3)
+
+        val conwayCubes = createConwayCubes(data, dimensions = 3)
         assertEquals(9, conwayCubes.keys.size)
         assertEquals(State.Inactive, conwayCubes[listOf(0,0,0)])
         assertEquals(State.Active, conwayCubes[listOf(1,0,0)])
@@ -24,6 +25,9 @@ class MainTest {
         assertEquals(3, conwayCubes.activeNeighbours(listOf(1,2,0),surroundingPositions(3)))
 
     }
+
+    val Position.z:Int get() = get(2)
+
     @Test
     fun `cycling through the data once gives the right result`() {
         val data = """
@@ -31,8 +35,7 @@ class MainTest {
             ..#
             ###
         """.trimIndent().split("\n")
-        val conwayCubes:ConwayCubes = mutableMapOf()
-        updateCube(conwayCubes,data, dimensions = 3)
+        val conwayCubes  = createConwayCubes(data, dimensions = 3)
         val newConwayCubes = conwayCubes.cycle(3)
         assertEquals(3, newConwayCubes.toList().filter{it.first.z == -1}.filter{it.second == State.Active }.count())
         assertEquals(5, newConwayCubes.toList().filter{it.first.z == 0}.filter{it.second == State.Active }.count())
@@ -45,9 +48,7 @@ class MainTest {
             ..#
             ###
         """.trimIndent().split("\n")
-        val conwayCubes:ConwayCubes = mutableMapOf()
-        updateCube(conwayCubes,data, dimensions = 3)
-        val newConwayCubes = conwayCubes.repeatCycles(3,3)
+        val newConwayCubes = data.process(3, 3)
         assertEquals(5, newConwayCubes.toList().filter{it.first.z == -2}.filter{it.second == State.Active }.count())
         assertEquals(10, newConwayCubes.toList().filter{it.first.z == -1}.filter{it.second == State.Active }.count())
         assertEquals(8, newConwayCubes.toList().filter{it.first.z == 0}.filter{it.second == State.Active }.count())
@@ -61,16 +62,12 @@ class MainTest {
             ..#
             ###
         """.trimIndent().split("\n")
-        val conwayCubes:ConwayCubes = mutableMapOf()
-        updateCube(conwayCubes,data, dimensions = 3)
-        val newConwayCubes = conwayCubes.repeatCycles(6,3)
+        val newConwayCubes = data.process(6, 3)
         assertEquals(112, newConwayCubes.toList().filter{it.second == State.Active }.count())
     }
     @Test
     fun `part one`() {
-        val conwayCubes:ConwayCubes = mutableMapOf()
-        updateCube(conwayCubes,day17Data, dimensions = 3)
-        val newConwayCubes = conwayCubes.repeatCycles(6,3)
+        val newConwayCubes = day17Data.process(6, 3)
         assertEquals(263, newConwayCubes.toList().filter{it.second == State.Active }.count())
     }
     @Test
@@ -80,30 +77,22 @@ class MainTest {
             ..#
             ###
         """.trimIndent().split("\n")
-        val conwayCubes:ConwayCubes = mutableMapOf()
-        updateCube(conwayCubes,data,4)
-        val newConwayCubes = conwayCubes.repeatCycles(6,4)
+        val newConwayCubes = data.process(6, 4)
         assertEquals(848, newConwayCubes.toList().filter{it.second == State.Active }.count())
     }
     @Test
     fun `part two`() {
-        val conwayCubesW:ConwayCubes = mutableMapOf()
-        updateCube(conwayCubesW,day17Data,4)
-        val newConwayCubes = conwayCubesW.repeatCycles(6, 4)
+        val newConwayCubes = day17Data.process(6, 4)
         assertEquals(1680, newConwayCubes.toList().filter{it.second == State.Active }.count())
     }
-    @Test
+    @Ignore
     fun `part two, 5 dimensions`() {
-        val conwayCubesW:ConwayCubes = mutableMapOf()
-        updateCube(conwayCubesW,day17Data,5)
-        val newConwayCubes = conwayCubesW.repeatCycles(6, 5)
+        val newConwayCubes = day17Data.process(6, 5)
         assertEquals(12440, newConwayCubes.toList().filter{it.second == State.Active }.count())
     }
     @Test
     fun `part two, 2 dimensions`() {
-        val conwayCubesW:ConwayCubes = mutableMapOf()
-        updateCube(conwayCubesW,day17Data,2)
-        val newConwayCubes = conwayCubesW.repeatCycles(6, 2)
+        val newConwayCubes = day17Data.process(6, 2)
         assertEquals(21, newConwayCubes.toList().filter{it.second == State.Active }.count())
     }
 }
