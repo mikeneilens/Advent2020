b968e72a9f46a3ad8aeb46b29d11c9b9d13ee35c
Mike Neilens <mike.neilens@gmail.com>
2020-12-18 13:57:28 +0000

    day18

--- day18/src/main/kotlin/Main.kt
+++ day18/src/main/kotlin/Main.kt
@@ -1,2 +1,68 @@
 
-fun main() = 0
+fun evaluate(expression:String):String {
+    val items = expression.split(" ")
+    return items.drop(1).chunked(2).fold(items.first().toLong()) { result, pair ->
+        if (pair[0] == "+") result + pair[1].toLong()
+        else result * pair[1].toLong()
+    }.toString()
+}
+
+fun findFirstExpression(expression:String):String {
+    var value:String = ""
+    expression.forEach { char ->
+        when(char) {
+            '(' -> value = ""
+            ')' -> return value
+            else -> value += char
+        }
+    }
+    return value
+}
+fun findFirstExpressionAndEvaluate(expression:String):String {
+    val firstExpression = findFirstExpression(expression)
+    val result = evaluate(firstExpression)
+    return if (firstExpression == expression) result.toString()
+    else expression.replace("($firstExpression)",result)
+}
+fun evaluateFull(expression:String):String = if (!expression.contains(' '))  expression
+    else evaluateFull(findFirstExpressionAndEvaluate(expression))
+
+fun partOne(data:List<String>) =
+    data.map(::evaluateFull).fold(0L){ acc,v -> acc + v.toLong()}
+
+//part two
+val operatorFunction:Map<String, (String, String)->String> = mapOf(
+    "+" to {a, b -> (a.toLong() + b.toLong()).toString()},
+    "*" to {a, b -> (a.toLong() * b.toLong()).toString()}
+)
+
+fun evaluate(expression:String, operator:String):String {
+    if (!expression.contains(operator)) return expression
+    val items = expression.split(" ")
+    var firstValue = items.first()
+    items.drop(1).chunked(2).forEach {
+        if (it[0] == operator ) {
+            val textExpresion =  "$firstValue $operator ${it[1]}"
+            val result = operatorFunction.getValue(operator)(firstValue, it[1])
+            return evaluate(expression.replaceFirst(textExpresion,result), operator)
+        } else {
+            firstValue = it[1]
+        }
+    }
+    return expression
+}
+
+fun findFirstExpressionAndEvaluate2(expression:String):String {
+    val firstExpression = findFirstExpression(expression)
+    val result = evaluate(evaluate(firstExpression,"+") ,"*")
+    return if (firstExpression == expression) result.toString()
+    else expression.replace("($firstExpression)",result)
+}
+
+fun evaluateFull2(expression:String):String = if (!expression.contains(' '))  expression
+else evaluateFull2(findFirstExpressionAndEvaluate2(expression))
+
+fun partTwo(data:List<String>) =
+    data.map(::evaluateFull2).fold(0L){ acc,v ->
+        println("$v");
+        acc + v.toLong()}
--- day18/src/test/kotlin/MainTest.kt
+++ day18/src/test/kotlin/MainTest.kt
@@ -3,7 +3,62 @@ import org.junit.jupiter.api.Test
 
 class MainTest {
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `test evaluating a simple expression`() {
+        assertEquals("71", evaluate("1 + 2 * 3 + 4 * 5 + 6"))
+    }
+    @Test
+    fun `finding first expression between braces`() {
+        assertEquals("1 + 2 * 3 + 4 * 5 + 6", findFirstExpression("1 + 2 * 3 + 4 * 5 + 6"))
+        assertEquals("2 * 3",findFirstExpression("1 + (2 * 3) + (4 * (5 + 6))"))
+    }
+    @Test
+    fun `find first expression and evaluate it`() {
+        assertEquals("71", findFirstExpressionAndEvaluate("1 + 2 * 3 + 4 * 5 + 6"))
+        assertEquals("1 + 6 + (4 * (5 + 6))",findFirstExpressionAndEvaluate("1 + (2 * 3) + (4 * (5 + 6))"))
+    }
+    @Test
+    fun `evaluate the whole expression`() {
+        assertEquals("71", evaluateFull("1 + 2 * 3 + 4 * 5 + 6"))
+        assertEquals("51",evaluateFull("1 + (2 * 3) + (4 * (5 + 6))"))
+        assertEquals("26",evaluateFull("2 * 3 + (4 * 5)"))
+        assertEquals("437",evaluateFull("5 + (8 * 3 + 9 + 3 * 4 * 3)"))
+        assertEquals("12240",evaluateFull("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))
+        assertEquals("13632",evaluateFull("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
+    }
+    @Test
+    fun `part one`() {
+        assertEquals(86311597203806, partOne(day18Data))
+    }
+
+
+    @Test
+    fun `evalaute plus in expression first`() {
+        assertEquals("3 * 7 * 11", evaluate("1 + 2 * 3 + 4 * 5 + 6", "+"))
+    }
+    @Test
+    fun `evaluating a simple expresion`() {
+        assertEquals("231", findFirstExpressionAndEvaluate2("1 + 2 * 3 + 4 * 5 + 6"))
+    }
+    @Test
+    fun `evaluate the whole expression using part 2 rules`() {
+        assertEquals("51",evaluateFull2("1 + (2 * 3) + (4 * (5 + 6))"))
+        assertEquals("46",evaluateFull2("2 * 3 + (4 * 5)"))
+        assertEquals("1445",evaluateFull2("5 + (8 * 3 + 9 + 3 * 4 * 3)"))
+        assertEquals("669060",evaluateFull2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))
+        assertEquals("23340",evaluateFull2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
+    }
+    @Test
+    fun `part two`() {
+        //276894780500845
+        //276894767062189
+        assertEquals(86311597203806, partTwo(day18Data))
+    }
+    @Test
+    fun `something `() {
+//        assertEquals("(7 * 8 * 9 + 57 * 8) * 7 + 8", findFirstExpressionAndEvaluate2("(7 * 8 * 9 + (9 * 6 + 3) * 8) * 7 + 8"))
+//        assertEquals("1060 * 7 + 8", findFirstExpressionAndEvaluate2("(7 * 8 * 9 + 57 * 8) * 7 + 8"))
+//        assertEquals("7428", findFirstExpressionAndEvaluate2("1060 * 7 + 8"))
+        //assertEquals("960", evaluate("7 * 8 * 9 + 57 * 8","*"))
+        assertEquals("6728", evaluateFull2("(7 * 8 * 9 + (9 * 6 + 3) * 8) * 7 + 8"))
     }
 }
