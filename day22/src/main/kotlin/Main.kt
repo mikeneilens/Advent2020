5abe1e8d63715da3bf255e8482bfdd5270ca2228
Mike Neilens <mike.neilens@gmail.com>
2020-12-22 22:32:30 +0000

    day 22

--- day22/src/main/kotlin/Main.kt
+++ day22/src/main/kotlin/Main.kt
@@ -1,91 +1,5 @@
 
-data class CardNode(val number: Int, var cardBelow: CardNode?)
-data class PlayersCards(var topCard: CardNode?, var bottomCard: CardNode?)
-data class Game(val hand1: PlayersCards, val hand2: PlayersCards) {
 
-    enum class Winner{ Player1, Player2}
-
-    val previousHands = mutableMapOf<String, Boolean>()
-
-    fun playRoundUntilWinner(count: Int = 1):Winner {
-        playRound()
-        if (hand1.topCard == null) {println("player 2 wins, count = $count"); return Winner.Player2}
-        if (hand2.topCard == null) {println("player 1 wins, count = $count"); return Winner.Player1}
-        return playRoundUntilWinner(count + 1)
-    }
-    fun playRound() {
-        val hand1Number = hand1.topCard?.number ?: - 1
-        val hand2Number = hand2.topCard?.number ?: - 1
-        when (true) {
-            hand1Number > hand2Number -> moveCardsHand2ToHand1()
-            hand2Number > hand1Number -> moveCardsHand1ToHand2()
-            else -> moveCardsToBottom()
-        }
-    }
-    fun playRoundUntilWinner2(count: Int = 1):Winner {
-        if (occuredAlready()) return Winner.Player1
-        previousHands["$hand1"] = true
-        previousHands["$hand2"] = true
-
-        playRound2()
-        if (hand1.topCard == null) return Winner.Player2
-        if (hand2.topCard == null) return Winner.Player1
-        return playRoundUntilWinner2(count + 1)
-    }
-
-    fun playRound2() {
-        val hand1Number = hand1.topCard?.number ?: - 1
-        val hand2Number = hand2.topCard?.number ?: - 1
-        when (true) {
-            (hand1.topCard!!.number < hand1.topCard!!.count() && hand2.topCard!!.number < hand2.topCard!!.count() ) -> {
-                val (hand1Top, hand1Bottom) = hand1.topCard!!.copyCardsBelow()
-                val (hand2Top, hand2Bottom) = hand2.topCard!!.copyCardsBelow()
-                val subGame = Game(PlayersCards(hand1Top, hand1Bottom), PlayersCards(hand2Top, hand2Bottom) )
-                val winner = subGame.playRoundUntilWinner2()
-                if (winner == Winner.Player1) moveCardsHand2ToHand1() else moveCardsHand1ToHand2()
-            }
-            hand1Number > hand2Number -> moveCardsHand2ToHand1()
-            hand2Number > hand1Number -> moveCardsHand1ToHand2()
-            else -> moveCardsToBottom()
-        }
-    }
-    fun occuredAlready() = (previousHands["$hand1"] != null) || (previousHands["$hand2"] != null)
-
-    fun moveCardsHand2ToHand1() {
-        val newTopCardFromHand = hand2.topCard?.cardBelow
-        val newTopCardToHand = hand1.topCard?.cardBelow
-        hand1.bottomCard?.cardBelow = hand1.topCard
-        hand1.bottomCard = hand1.topCard
-        hand1.bottomCard?.cardBelow = hand2.topCard
-        hand1.bottomCard = hand2.topCard
-        hand1.bottomCard?.cardBelow = null
-        hand2.topCard = newTopCardFromHand
-        hand1.topCard = newTopCardToHand
-    }
-    fun moveCardsHand1ToHand2() {
-        val newTopCardFromHand = hand1.topCard?.cardBelow
-        val newTopCardToHand = hand2.topCard?.cardBelow
-        hand2.bottomCard?.cardBelow = hand2.topCard
-        hand2.bottomCard = hand2.topCard
-        hand2.bottomCard?.cardBelow = hand1.topCard
-        hand2.bottomCard = hand1.topCard
-        hand2.bottomCard?.cardBelow = null
-        hand1.topCard = newTopCardFromHand
-        hand2.topCard = newTopCardToHand
-    }
-    fun moveCardsToBottom() {
-        val newTopCardHand1 = hand1.topCard?.cardBelow
-        val newTopCardHand2 = hand2.topCard?.cardBelow
-        hand1.bottomCard?.cardBelow = hand1.topCard
-        hand1.bottomCard = hand1.topCard
-        hand1.bottomCard?.cardBelow = null
-        hand2.bottomCard?.cardBelow = hand2.topCard
-        hand2.bottomCard = hand2.topCard
-        hand2.bottomCard?.cardBelow = null
-        hand1.topCard = newTopCardHand1
-        hand2.topCard = newTopCardHand2
-    }
-}
 
 
 fun List<String>.parse():List<List<Int>> {
@@ -102,53 +16,78 @@ fun List<String>.parse():List<List<Int>> {
     return cardDecks
 }
 
-fun List<String>.getHands():Game {
-    val cardDecks = parse()
-    return Game(cardDecks[0].makeHand(), cardDecks[1].makeHand())
-}
+fun List<String>.process():Pair<List<Int>,List<Int>> {
 
-fun List<Int>.makeHand():PlayersCards {
-    val topCard = CardNode(first(), null)
-    var bottomCard = topCard
-    drop(1).forEach{ num ->
-        val newBottomCard = CardNode(num, null)
-        bottomCard.cardBelow = newBottomCard
-        bottomCard = newBottomCard
+    val deck1 = parse()[0].toMutableList()
+    val deck2 = parse()[1].toMutableList()
+
+    while (deck1.isNotEmpty() && deck2.isNotEmpty()) {
+        if (deck1.first() > deck2.first()) {
+            deck1.add(deck1.first())
+            deck1.add(deck2.first())
+        } else {
+            deck2.add(deck2.first())
+            deck2.add(deck1.first())
+        }
+        deck1.removeAt(0)
+        deck2.removeAt(0)
     }
-    return PlayersCards(topCard, bottomCard)
-}
-fun CardNode.count():Int {
-    if (cardBelow == null) return 1
-    else return 1 + (cardBelow?.count() ?: 1)
-}
-fun CardNode.calcValue():Long {
-    if (cardBelow == null) return count() * number.toLong()
-    else return count() * number + (cardBelow?.calcValue() ?: 0)
+    return Pair(deck1, deck2)
 }
 
-fun CardNode.highestCardBelowTopCard():Int {
-    var topCard:CardNode? = this.cardBelow
-    var maxNumber = 0
-    while (topCard?.cardBelow != null) {
-        if (topCard.number > maxNumber) maxNumber = topCard.number
-        topCard = topCard.cardBelow
+fun List<String>.process2():Triple<String,List<Int>,List<Int>> {
+
+    val initialDeck1 = parse()[0].toList()
+    val initialDeck2 = parse()[1].toList()
+
+    fun process(d1:List<Int>, d2:List<Int>, depth:Int = 0):Triple<String,List<Int>,List<Int>> {
+        val history = mutableSetOf<List<Int>>()
+        val deck1 = d1.toMutableList()
+        val deck2 = d2.toMutableList()
+
+        while (deck1.isNotEmpty() && deck2.isNotEmpty()) {
+
+            if ( history.contains(deck1) || history.contains(deck2) ) {
+                return Triple("P1",deck1, deck2)
+            }
+
+            history.add(deck1)
+            history.add(deck2)
+
+            if (deck1.first() < deck1.size && deck2.first() < deck2.size ) {
+                val (winner, _, _) = process(deck1.drop(1).take(deck1.first()), deck2.drop(1).take(deck2.first()), depth + 1)
+                if (winner == "P1") {
+                    deck1.add(deck1.first())
+                    deck1.add(deck2.first())
+                } else {
+                    deck2.add(deck2.first())
+                    deck2.add(deck1.first())
+                }
+            } else {
+                if (deck1.first() > deck2.first()) {
+                    deck1.add(deck1.first())
+                    deck1.add(deck2.first())
+                } else {
+                    deck2.add(deck2.first())
+                    deck2.add(deck1.first())
+                }
+            }
+            deck1.removeAt(0)
+            deck2.removeAt(0)
+        }
+        return if (deck1.isNotEmpty()) Triple("P1",deck1, deck2)
+        else Triple("P2",deck1, deck2)
     }
-    if (topCard!!.number > maxNumber) maxNumber = topCard!!.number
-    return maxNumber
+
+    return process(initialDeck1, initialDeck2)
 }
 
-
-fun CardNode.copyCardsBelow():Pair<CardNode,CardNode?> {
-    val maxQty = number
-    var topCard:CardNode? = this.cardBelow
-    var newTopCard = CardNode((topCard?.number ?: -1), null)
-    var nextNewCard:CardNode? = newTopCard
-    var qtyCopied = 1
-    while (topCard?.cardBelow != null && qtyCopied < maxQty) {
-        topCard = topCard.cardBelow
-        nextNewCard?.cardBelow =  topCard?.let{ CardNode(it.number, null) }
-        nextNewCard = nextNewCard?.cardBelow
-        qtyCopied++
+fun List<Int>.score():Long {
+    var i = 0
+    var sum = 0L
+    while (i <= lastIndex) {
+        sum += get(i) * (size - i)
+        i++
     }
-    return Pair(newTopCard, nextNewCard)
+    return sum
 }
