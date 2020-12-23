0d7895289c61b34c7a5ae42b104b650c4a66b490
Mike Neilens <mike.neilens@gmail.com>
2020-12-23 18:11:14 +0000

    day 23 refactor

--- day23/src/main/kotlin/PartTwo.kt
+++ day23/src/main/kotlin/PartTwo.kt
@@ -1,6 +1,63 @@
 
+val mapOfCupsForLabels = mutableMapOf<Int, Cup>()
 
-class Cup(val label:Int, var next:Cup?=null )
+class Cup(val label:Int, var next:Cup?=null ) {
+
+    fun toListOfInts():List<Int> {
+        val list = mutableListOf(label)
+        var nextCup = next
+        while (nextCup != this && nextCup!= null) {
+            list.add(nextCup.label)
+            nextCup = nextCup.next
+        }
+        return list
+    }
+
+    fun pickupThreeCups():Cup {
+        val cup1 = next
+        val cup2 = cup1?.next
+        val cup3 = cup2?.next
+        this.next = cup3?.next
+        cup3?.next = null
+        return cup1!!
+    }
+
+    fun contains(otherLabel:Int, firstCup:Cup? = null):Boolean {
+        if (label == otherLabel ) return true
+        if (label == firstCup?.label) return false
+        if ( next == null) return false
+        return next!!.contains(otherLabel, firstCup ?: this )
+    }
+
+    fun destinationCup(threeCups:Cup, maxSize:Int = 9):Cup {
+        val destinationLabel = getDestinationLabel(threeCups, maxSize)
+        val cupInMap = mapOfCupsForLabels[destinationLabel]
+        return if (cupInMap != null) cupInMap
+        else {
+            val cupInList = getCupForLabel(destinationLabel)
+            mapOfCupsForLabels[destinationLabel] = cupInList
+            cupInList
+        }
+    }
+
+    fun getDestinationLabel(threeCups: Cup, maxSize: Int) =
+        if (!threeCups.contains(label.labelMinus(1, maxSize)))
+            label.labelMinus(1, maxSize)
+        else if (!threeCups.contains(label.labelMinus(2, maxSize)))
+            label.labelMinus(2, maxSize)
+        else if (!threeCups.contains(label.labelMinus(3, maxSize)))
+            label.labelMinus(3, maxSize)
+        else
+            label.labelMinus(4, maxSize)
+
+    fun playRound(maxSize:Int = 9):Cup {
+        val threeCups = pickupThreeCups()
+        val destinationCup = destinationCup(threeCups, maxSize)
+        threeCups.next!!.next!!.next = destinationCup.next
+        destinationCup.next = threeCups
+        return this.next!!
+    }
+}
 
 fun List<Int>.toCup():Cup {
     val firstCup = Cup(first())
@@ -15,42 +72,6 @@ fun List<Int>.toCup():Cup {
     prevCup?.next = firstCup
     return firstCup
 }
-fun Cup.toListOfInts():List<Int> {
-    val list = mutableListOf(label)
-    var nextCup = next
-    while (nextCup != this && nextCup!= null) {
-        list.add(nextCup.label)
-        nextCup = nextCup.next
-    }
-    return list
-}
-fun Cup.pickupThreeCups():Cup {
-    val cup1 = next
-    val cup2 = cup1?.next
-    val cup3 = cup2?.next
-    this.next = cup3?.next
-    cup3?.next = null
-    return cup1!!
-}
-tailrec fun Cup.contains(otherLabel:Int, firstCup:Cup? = null):Boolean {
-    if (label == otherLabel ) return true
-    if (label == firstCup?.label) return false
-    if ( next == null) return false
-    return next!!.contains(otherLabel, firstCup ?: this )
-}
-
-val mapOfCupsForLabels = mutableMapOf<Int, Cup>()
-
-fun Cup.destinationCup(threeCups:Cup, maxSize:Int = 9):Cup {
-    val destinationLabel = getDestinationLabel(threeCups, maxSize)
-    val cupInMap = mapOfCupsForLabels[destinationLabel]
-    return if (cupInMap != null) cupInMap
-    else {
-        val cupInList = getCupForLabel(destinationLabel)
-        mapOfCupsForLabels[destinationLabel] = cupInList
-        cupInList
-    }
-}
 
 tailrec fun Cup.getCupForLabel(otherLabel:Int, firstCup:Cup? = null):Cup{
     if (label == otherLabel) return this
@@ -59,28 +80,9 @@ tailrec fun Cup.getCupForLabel(otherLabel:Int, firstCup:Cup? = null):Cup{
     else next!!.getCupForLabel(otherLabel, firstCup ?: this )
 }
 
-fun Cup.getDestinationLabel(threeCups: Cup, maxSize: Int) =
-    if (!threeCups.contains(label.labelMinus(1, maxSize)))
-        label.labelMinus(1, maxSize)
-    else if (!threeCups.contains(label.labelMinus(2, maxSize)))
-        label.labelMinus(2, maxSize)
-    else if (!threeCups.contains(label.labelMinus(3, maxSize)))
-        label.labelMinus(3, maxSize)
-    else
-        label.labelMinus(4, maxSize)
-
-
 fun Int.labelMinus(value:Int, maxSize:Int):Int {
     val newLabel = this - value
     return if (newLabel > 0) newLabel else maxSize + newLabel
-}
-
-fun Cup.playRound(maxSize:Int = 9):Cup {
-    val threeCups = pickupThreeCups()
-    val destinationCup = destinationCup(threeCups, maxSize)
-    threeCups.next!!.next!!.next = destinationCup.next
-    destinationCup.next = threeCups
-    return this.next!!
 }
 
 fun process(data:List<Int>, times:Int = 10):Cup {
