85c13ed9595bc845cc43d3fccb09695ae94681e1
Mike Neilens <mike.neilens@gmail.com>
2020-12-24 14:19:16 +0000

    day 24 part one

--- day24/src/main/kotlin/Main.kt
+++ day24/src/main/kotlin/Main.kt
@@ -1,2 +1,53 @@
+const val white = "white"
+const val black = "black"
 
-fun main() = 0
+data class Vector(val x:Int, val y:Int) {
+    operator fun plus(other:Vector) = Vector(this.x + other.x, this.y + other.y)
+}
+
+enum class Step(val move:Vector) {
+    East  (Vector(2,0)),
+    SouthEast (Vector(1,1) ),
+    SouthWest (Vector(-1,1)),
+    West  (Vector(-2,0)),
+    NorthWest (Vector(-1,-1)),
+    NorthEast (Vector(1,-1))
+}
+fun String.toStep() = when(true) {
+    startsWith("e") -> Pair(Step.East,removePrefix("e"))
+    startsWith("se") -> Pair(Step.SouthEast,removePrefix("se"))
+    startsWith("sw") -> Pair(Step.SouthWest,removePrefix("sw"))
+    startsWith("w") -> Pair(Step.West,removePrefix("w"))
+    startsWith("nw") -> Pair(Step.NorthWest,removePrefix("nw"))
+    startsWith("ne") -> Pair(Step.NorthEast,removePrefix("ne"))
+    else -> Pair(Step.East,removePrefix("e"))
+}
+fun String.toSteps():List<Step> {
+    var string = this
+    val result = mutableListOf<Step>()
+    while (string.isNotEmpty()) {
+        val (step, newString) = string.toStep()
+        string = newString
+        result.add(step)
+    }
+    return result
+}
+fun List<Step>.findTilePosition():Vector {
+    var position = Vector(0,0)
+    forEach { step ->
+        position += step.move
+    }
+    return position
+}
+
+fun List<String>.flipTiles():Map<Vector,String> {
+    val map = mutableMapOf<Vector, String>()
+    forEach { line ->
+        val tilePosition = line.toSteps().findTilePosition()
+        val tileColor = map[tilePosition] ?: white
+        map[tilePosition] = if (tileColor == white) black else white
+    }
+    return map
+}
+
+fun Map<Vector,String>.blackTiles() = values.filter{ it == black}.size
--- day24/src/test/kotlin/MainTest.kt
+++ day24/src/test/kotlin/MainTest.kt
@@ -2,8 +2,48 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MainTest {
+    val sampleData = """
+        sesenwnenenewseeswwswswwnenewsewsw
+        neeenesenwnwwswnenewnwwsewnenwseswesw
+        seswneswswsenwwnwse
+        nwnwneseeswswnenewneswwnewseswneseene
+        swweswneswnenwsewnwneneseenw
+        eesenwseswswnenwswnwnwsewwnwsene
+        sewnenenenesenwsewnenwwwse
+        wenwwweseeeweswwwnwwe
+        wsweesenenewnwwnwsenewsenwwsesesenwne
+        neeswseenwwswnwswswnw
+        nenwswwsewswnenenewsenwsenwnesesenew
+        enewnwewneswsewnwswenweswnenwsenwsw
+        sweneswneswneneenwnewenewwneswswnese
+        swwesenesewenwneswnwwneseswwne
+        enesenwswwswneneswsenwnewswseenwsese
+        wnwnesenesenenwwnenwsewesewsesesew
+        nenewswnwewswnenesenwnesewesw
+        eneswnwswnwsenenwnwnwwseeswneewsenese
+        neswnwewnwnwseenwseesewsenwsweewe
+        wseweeenwnesenwwwswnew
+    """.trimIndent().split("\n")
+
     @Test
-    fun `a test`() {
-        assertEquals(0,main())
+    fun `test parsing a string into a list of steps`() {
+        val result = "eseswwnwne".toSteps()
+        val expectedResult = listOf(Step.East,Step.SouthEast,Step.SouthWest,Step.West,Step.NorthWest,Step.NorthEast)
+        assertEquals(expectedResult, result)
+    }
+    @Test
+    fun `nwwswee returns to self`() {
+        val endingPosition = "nwwswee".toSteps().findTilePosition()
+        assertEquals(Vector(0,0), endingPosition)
+    }
+    @Test
+    fun `flipping tiles from sample data`() {
+        val noOfBlackTiles = sampleData.flipTiles().blackTiles()
+        assertEquals(10, noOfBlackTiles)
+    }
+    @Test
+    fun `part one`() {
+        val noOfBlackTiles = day24Data.flipTiles().blackTiles()
+        assertEquals(521, noOfBlackTiles)
     }
 }
