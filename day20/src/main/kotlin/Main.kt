142c34c2c878699d701a01664315c33d14e5ea88
Mike Neilens <mike.neilens@gmail.com>
2020-12-27 19:52:19 +0000

    day20

--- day20/src/main/kotlin/Main.kt
+++ day20/src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 import kotlin.math.sqrt
-import kotlin.random.Random
 
-data class Tile(val ref:String, val lines:List<String>, var OrienttionNo:Int = 0) {
+data class Tile(val ref:String, var lines:List<String>, var used:Boolean = false) {
     fun edges() = listOf(
         lines[0],
         lines.map { it.last() }.joinToString(""),
@@ -31,6 +30,15 @@ data class Tile(val ref:String, val lines:List<String>, var OrienttionNo:Int = 0
             }
         }
 
+    fun flip():Tile {
+        lines = lines.reversed()
+        return this
+    }
+    fun rotate():Tile {
+        val column = {col:Int -> lines.map{it[col]}.joinToString("") }
+        lines =  lines.mapIndexed { index,_ -> column(index).reversed()  }
+        return this
+    }
 }
 
 data class Match(val tile:Tile, val edge:Int, val otherTile:Tile, val otherEdge:Int)
@@ -42,9 +50,77 @@ fun allMatchesBetweenAllTiles(tiles:List<Tile>):List<Match> =
     }.filter{it.tile != it.otherTile}
 
 fun cornerTiles(allMatches:List<Match>) = allMatches.groupBy {it.tile}.filter{it.value.size == 2 }.flatMap{it.value}
+fun edgeTiles(allMatches:List<Match>) = allMatches.groupBy {it.tile}.filter{it.value.size == 3 }.flatMap{it.value}
 
-fun topLeftCorner(cornerTile:List<Match>) {
+fun topLeftCorner(cornerTiles:List<Match>) =
+cornerTiles.groupBy { it.tile }.filter{ it.value.first().edge == 1 && it.value.last().edge == 2 }.map{it.key}.first()
 
+fun Tile.toRight(matches:List<Match>) =
+    matches.first{ it.tile == this && it.edge == 1 }
+
+fun Tile.below(matches:List<Match>) =
+    matches.first{ it.tile == this && it.edge == 2 }
+
+
+
+fun Tile.setEdge(edge:Int, edgeToMatch:String) {
+    (1..4).forEach {
+        if (this.edges()[edge] == edgeToMatch.reversed()) return
+        rotate()
+    }
+    flip()
+    (1..4).forEach {
+        if (this.edges()[edge] == edgeToMatch.reversed()) return
+        rotate()
+    }
+}
+
+fun createPicture(tiles:List<Tile>):List<Tile> {
+    val width = sqrt(tiles.size.toDouble()).toInt()
+    val picture = mutableListOf<Tile>()
+    val allMatches = allMatchesBetweenAllTiles(tiles)
+    val cornerTiles = cornerTiles((allMatches))
+    val edgeTiles = edgeTiles((allMatches))
+    val matchesContainingEdges = allMatches.filter{edgeTiles.map{it.tile}.contains(it.otherTile)}
+    val matchesContainingCorners = allMatches.filter{cornerTiles.map{it.tile}.contains(it.otherTile)}
+
+
+    (1..width).forEach{
+        if (it == 1) {
+            val firstTileOnLine = topLeftCorner(cornerTiles)
+            firstTileOnLine.used = true
+            picture.add(firstTileOnLine)
+
+            picture.completeLine(matchesContainingEdges, width - 2)
+
+            val (_, _, lastTileOnRow, _) = picture.last().toRight(matchesContainingCorners)
+            lastTileOnRow.setEdge(3, picture.last().edges()[1])
+            lastTileOnRow.used
+            picture.add(lastTileOnRow)
+        } else {
+            val (_, _, firstTileOnLine, _) = picture[picture.size - width].below(allMatches)
+            firstTileOnLine.setEdge(0, picture[picture.size - width].edges()[2])
+            firstTileOnLine.used = true
+            picture.add(firstTileOnLine)
+            picture.completeLine(allMatches, width - 2)
+            val (_, _, lastTileOnRow, _) = picture.last().toRight(allMatches)
+            lastTileOnRow.setEdge(3, picture.last().edges()[1])
+            lastTileOnRow.used
+            picture.add(lastTileOnRow)
+        }
+    }
+
+    return picture
+}
+
+fun MutableList<Tile>.completeLine(matches:List<Match>, qty:Int ) {
+    if (qty == 0) return
+    val lastTile = last()
+    val (_, _, nextTile, _) = lastTile.toRight(matches)
+    nextTile.setEdge(3, lastTile.edges()[1])
+    nextTile.used
+    add(nextTile)
+    completeLine(matches, qty - 1)
 }
 
 
--- day20/src/test/kotlin/MainTest.kt
+++ day20/src/test/kotlin/MainTest.kt
@@ -11,7 +11,7 @@ class MainTest {
         assertEquals("..###..###", tiles[0].lines[9])
     }
     @Test
-    fun `edges`() {
+    fun `find edges of a tile`() {
         val tileData = listOf(
             "abcd", "lxxe", "kxxf", "jihg",
         )
@@ -54,6 +54,16 @@ class MainTest {
         assertEquals(20899048083289L, result)
     }
     @Test
+    fun `find edge tiles`() {
+        val tiles = sampleData.parse()
+        val edgeTiles: List<Match> = edgeTiles(allMatchesBetweenAllTiles(tiles))
+        assertTrue(edgeTiles.map{it.tile.ref}.contains("2311"))
+        assertTrue(edgeTiles.map{it.tile.ref}.contains("2729"))
+        assertTrue(edgeTiles.map{it.tile.ref}.contains("2473"))
+        assertTrue(edgeTiles.map{it.tile.ref}.contains("1489"))
+        assertEquals(4, edgeTiles.map{it.tile.ref}.distinct().size)
+    }
+    @Test
     fun `part one`() {
         val tiles = day20Data.parse()
         val cornerTiles: List<Match> = cornerTiles(allMatchesBetweenAllTiles(tiles))
@@ -61,5 +71,76 @@ class MainTest {
         val result = cornerTiles.map{it.tile.ref.toInt()}.distinct().fold(1L){total, value -> total * value.toLong()}
         assertEquals(16937516456219L, result)
     }
+    @Test
+    fun `find top left corner tile`() {
+        val tiles = sampleData.parse()
+        val cornerTiles: List<Match> = cornerTiles(allMatchesBetweenAllTiles(tiles))
+        val topLeft = topLeftCorner(cornerTiles)
+        assertEquals("2971", topLeft.ref)
+    }
+    @Test
+    fun `find tile to the right of a tile`() {
+        val tiles = sampleData.parse()
+        val matches = allMatchesBetweenAllTiles(tiles)
+        val cornerTiles: List<Match> = cornerTiles(matches)
+        val topLeft = topLeftCorner(cornerTiles)
 
+        val (_, _, tileToRight, matchingEdge) = topLeft.toRight(matches)
+        assertEquals("1489", tileToRight.ref)
+        assertEquals(7, matchingEdge)
+    }
+    @Test
+    fun `find tile below a tile`() {
+        val tiles = sampleData.parse()
+        val matches = allMatchesBetweenAllTiles(tiles)
+        val cornerTiles: List<Match> = cornerTiles(matches)
+        val topLeft = topLeftCorner(cornerTiles)
+
+        val (_, _, below, matchingEdge) = topLeft.below(matches)
+        assertEquals("2729", below.ref)
+        assertEquals(4, matchingEdge)
+    }
+    @Test
+    fun `rotating a tile`() {
+        val tileData = listOf("1234", "5678", "9abc", "defg",)
+        val tile = Tile("test",tileData)
+        tile.rotate()
+        assertEquals(listOf("d951","ea62","fb73","gc84"), tile.lines)
+        tile.rotate()
+        tile.rotate()
+        tile.rotate()
+        assertEquals(listOf("1234", "5678", "9abc", "defg",), tile.lines)
+    }
+    @Test
+    fun `tiles is rotated so left edge matches the right edge of the tile before it`() {
+        val tiles = sampleData.parse()
+        val matches = allMatchesBetweenAllTiles(tiles)
+        val cornerTiles: List<Match> = cornerTiles(matches)
+        val topLeft = topLeftCorner(cornerTiles)
+        val topLeftRightEdge = topLeft.edges()[1]
+        val (_, _, tileToRight, matchingEdge) = topLeft.toRight(matches)
+        tileToRight.setEdge(3, topLeftRightEdge)
+        assertEquals("#.#.##...#", tileToRight.edges()[3])
+    }
+    @Test
+    fun `create picture`() {
+        val tiles = sampleData.parse()
+        val picture = createPicture(tiles)
+        assertEquals("2971", picture[0].ref)
+        assertEquals("1489", picture[1].ref)
+        assertEquals("1171", picture[2].ref)
+        assertEquals("2729", picture[3].ref)
+        assertEquals("1427", picture[4].ref)
+        assertEquals("2473", picture[5].ref)
+        assertEquals("1951", picture[6].ref)
+        assertEquals("2311", picture[7].ref)
+        assertEquals("3079", picture[8].ref)
+    }
+    @Test
+    fun `create picture using full data`() {
+        val tiles = day20Data.parse()
+        val picture = createPicture(tiles)
+        assertEquals("1747", picture[0].ref)
+        assertEquals("1747", picture[8].ref)
+    }
 }
